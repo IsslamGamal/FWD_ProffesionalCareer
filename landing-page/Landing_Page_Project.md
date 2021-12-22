4e959e0581413608e2fbb950d0409ecf02cf1625
IsslamGamal <47702112+IsslamGamal@users.noreply.github.com>
2021-12-22 23:54:44 +0200

    Landing Page: Final version of the project
    This commit contains the final stable and tested version of the project that meet all the requirements.

---
+++ landing-page/Landing_Page_Project.md
@@ -0,0 +1,27 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Description](#description)
+* [Requirements](#requirements)
+* [Acknowledgment](#acknowledgment)
+
+## Description
+It's a landing page project that contains some feature related to menu navigation 
+and loading page content in addition to some existing animation while view/scrolling
+the page content.
+
+## Requirements
+
+1. `Navigation Menu` has to be build *dynamically* using `JS`
+
+2. The currently Viewd section and the corresponding menu item has to be `highlighted`.
+
+3. Smooth `scrolling` has to be applied in case of clicking any menu item.
+
+4. Navigation menu should be `responsive` to the page size.
+
+
+## Acknowledgment
+Thanks all of our chort moderatoers for the great effort for supporting us to get the right path in or career.
+Special thanks for Eng Islam for the greate session of walking through the project, it was really helpfull.
--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,16 +1,16 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
@@ -20,14 +20,21 @@
 
 /**
  * Define Global Variables
- * 
+ *
 */
+/* !Description: Store the Section List in a variable */ 
+const aSectionsList = document.querySelectorAll('section');
 
+/* !Description: Create the Document Fragement */ 
+const documentFragmentPage = document.createDocumentFragment();
 
+const buttonElement = document.querySelector('.icon');
+
+const menu = document.querySelector('#navbar__list');
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -35,11 +42,91 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
-// build the nav
+/* !Description: The function of Highlighting the Currently Viewed section */
+function scrollHighlighting ()
+{
+    for (section of aSectionsList)
+    {
 
+        if (section.getBoundingClientRect().top >=-400 &&
+            section.getBoundingClientRect().top <=200)
+        {
+            section.classList.add('your-active-class');
+            section.querySelector('h2').classList.add('your-active-class');
+
+        }
+        else 
+        {
+            section.classList.remove('your-active-class');
+            section.querySelector('h2').classList.remove('your-active-class');
+
+        }
+    }
+}
+
+
+function navMenuCreateFunc()
+{
+    /* Note: Create the UL Element that will contains the menu list Items */ 
+    const ulMenuElement = document.querySelector('#navbar__list');
+
+    /* !COmment: Create the Temp menu Element Variable */
+    let singleListElement;
+    /* !Comment: Create the Temp Menu Link Element */
+    let  anchorLink;
+
+    ulMenuElement.classList.add('navbar__menu');
+
+    /* !Note: Create the List dynamicly based on the Number of the implemented Sections */
+    for (section of aSectionsList)
+    {
+        /* !Note: Create a List element */ 
+        singleListElement = document.createElement('li');
+        singleListElement.classList.add('navbar__menu');
+
+        /* !Note: Create a Link that will refere to the coresponding Section */
+        anchorLink = document.createElement('a');
+
+        /* !Note: Add the Acnhor Link Classes */ 
+        anchorLink.classList.add('navbar__menu');
+        anchorLink.classList.add('menu__link');
+
+        /* !Note: Add the Hyper Reference of Sections to the Link Text */ 
+        anchorLink.href = `#${section.getAttribute('id')}`;
+
+        /* !Note: Give the Link name of the Corresponding Section dynamicly */ 
+        anchorLink.textContent = section.dataset.nav; 
+
+        anchorLink.addEventListener('click', function smothSectionScroll( currEvent )
+        {
+            let currSection;
+            /* !Comment: Stop the Default action of the Event */ 
+            currEvent.preventDefault();
+
+            /* !Comment: Get the Section name by extracting it from it's href and cut the '#' sign */
+            currSection = document.querySelector(`#${currEvent.target.href.split('#')[1]}`);
+            
+            /* !Comment: Set the Scrolling Propertise */ 
+            currSection.scrollIntoView(
+                {
+                    behavior: 'smooth',
+                    block: 'start'
+                }
+            )
+        });
+        /* !Note: Add the link to the List element */
+        singleListElement.appendChild(anchorLink);
+        /* !Note: Add the List element to the UL element */
+        ulMenuElement.appendChild(singleListElement);
+    }
+
+    documentFragmentPage.appendChild(ulMenuElement);
+    
+    document.querySelector('nav').appendChild(documentFragmentPage);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -50,13 +137,23 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
+
+/* !Description: Main Event of creating the navigator menu on Page Content loading */ 
+document.addEventListener('DOMContentLoaded', navMenuCreateFunc)
 
 // Scroll to section on link click
 
-// Set sections as active
+/* !Description: The event of highlighting the current text attached to the navigator */ 
+window.addEventListener('scroll', scrollHighlighting);
 
 
+
+/* !Description: Activate the menu Event */ 
+buttonElement.addEventListener('click', function ()
+{
+    menu.classList.toggle('show');
+})
